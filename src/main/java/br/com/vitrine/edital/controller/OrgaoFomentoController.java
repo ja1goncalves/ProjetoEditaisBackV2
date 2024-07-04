def037742640e6d6524f86a5ddedaf2ce1a8386e
fgpacheco <felipeguimaraes540@gmail.com>
2024-07-04 00:18:56 -0300

    Inserção do Swagger 3 com o OpenAPI para documentação da API do Dashboard de editais

--- src/main/java/br/com/vitrine/edital/controller/OrgaoFomentoController.java
+++ src/main/java/br/com/vitrine/edital/controller/OrgaoFomentoController.java
@@ -1,13 +1,23 @@
 package br.com.vitrine.edital.controller;
 
 import br.com.vitrine.edital.model.dto.OrgaoFomentoDTO;
+import br.com.vitrine.edital.model.dto.ResponseExceptionDTO;
+import br.com.vitrine.edital.model.dto.UsuarioDTO;
 import br.com.vitrine.edital.service.interfaces.OrgaoFomentoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Órgão Fomento", description = "Gerencimento de Órgãos de Formento")
 @RestController
 @RequestMapping(path = "/orgaoFomento")
 public class OrgaoFomentoController {
@@ -18,6 +28,14 @@ public class OrgaoFomentoController {
         this.orgaoFomentoService = orgaoFomentoService;
     }
 
+    @Operation(
+            summary = "Cadastrar um novo órgão de fomento",
+            description = "Este endpoint tem como objetivo cadastrar um novo órgão de fomento.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", content = {@Content(schema = @Schema(implementation = OrgaoFomentoDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "409", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "422", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")})})
     @PostMapping
     public ResponseEntity<OrgaoFomentoDTO> create(@RequestBody OrgaoFomentoDTO orgaoFomentoDTO) {
         return ResponseEntity
@@ -26,28 +44,64 @@ public class OrgaoFomentoController {
 
     }
 
+    @Operation(
+            summary = "Atualizar um órgão de fomento",
+            description = "Este endpoint tem como objetivo atualizar um órgão de fomento já presente no banco de dados.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = OrgaoFomentoDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "409", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "422", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")})})
     @PutMapping
     public ResponseEntity<OrgaoFomentoDTO> update(@RequestBody OrgaoFomentoDTO orgaoFomentoDTO) {
         orgaoFomentoDTO = orgaoFomentoService.update(orgaoFomentoDTO);
         return ResponseEntity.ok().body(orgaoFomentoDTO);
     }
 
+    @Operation(
+            summary = "Obter um órgão de fomento por ID",
+            description = "Este endpoint tem como objetivo obter um órgão de fomento através do seu ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = OrgaoFomentoDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")})})
     @GetMapping(value = "/{id}")
     public ResponseEntity<OrgaoFomentoDTO> recover(@PathVariable("id") Long idOrgaoFomento) {
         return ResponseEntity.ok().body(orgaoFomentoService.recover(idOrgaoFomento));
     }
 
+    @Operation(
+            summary = "Obter um órgão de fomento por seu nome",
+            description = "Este endpoint tem como objetivo obter um órgão de fomento através do seu nome.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = OrgaoFomentoDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")})})
     @GetMapping(value = "/nome/{nome}")
     public ResponseEntity<OrgaoFomentoDTO> recoverByName(@PathVariable("nome") String nomeOrgaoFomento) {
         return ResponseEntity.ok().body(orgaoFomentoService.recoverByName(nomeOrgaoFomento));
     }
 
+    @Operation(
+            summary = "Deletar um órgão de fomento por ID",
+            description = "Este endpoint tem como objetivo deletar um órgão de fomento através do seu ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")})})
     @DeleteMapping(value = "/{id}")
     public ResponseEntity delete(@PathVariable("id") Long idOrgaoFomento) {
         orgaoFomentoService.delete(idOrgaoFomento);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @Operation(
+            summary = "Obter todos os órgãos de fomento",
+            description = "Este endpoint tem como objetivo obter todos os órgãos de fomento cadastrados no banco de dados.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = UsuarioDTO.class)))}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")})})
     @GetMapping
     public ResponseEntity<List<OrgaoFomentoDTO>> getAll() {
         return ResponseEntity.ok().body(orgaoFomentoService.getAll());
--- src/main/java/br/com/vitrine/edital/controller/UsuarioController.java
+++ src/main/java/br/com/vitrine/edital/controller/UsuarioController.java
@@ -1,13 +1,22 @@
 package br.com.vitrine.edital.controller;
 
+import br.com.vitrine.edital.model.dto.ResponseExceptionDTO;
 import br.com.vitrine.edital.model.dto.UsuarioDTO;
 import br.com.vitrine.edital.service.interfaces.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Usuário", description = "Gerencimento de Usuários")
 @RestController
 @RequestMapping(path = "/usuario")
 public class UsuarioController {
@@ -18,6 +27,14 @@ public class UsuarioController {
         this.usuarioService = usuarioService;
     }
 
+    @Operation(
+            summary = "Cadastrar um novo usuário",
+            description = "Este endpoint tem como objetivo cadastrar um novo usuário no banco de dados.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", content = {@Content(schema = @Schema(implementation = UsuarioDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "409", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "422", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")})})
     @PostMapping
     public ResponseEntity<UsuarioDTO> create(@RequestBody UsuarioDTO usuarioDTO) {
         return ResponseEntity
@@ -26,32 +43,71 @@ public class UsuarioController {
 
     }
 
+    @Operation(
+            summary = "Atualizar um usuário",
+            description = "Este endpoint tem como objetivo atualizar um usuário já presente no banco de dados.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = UsuarioDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "409", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "422", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")})})
     @PutMapping
     public ResponseEntity<UsuarioDTO> update(@RequestBody UsuarioDTO usuarioDTO) {
         usuarioDTO = usuarioService.update(usuarioDTO);
         return ResponseEntity.ok().body(usuarioDTO);
     }
 
+    @Operation(
+            summary = "Obter um usuário por ID",
+            description = "Este endpoint tem como objetivo obter um usuário através do seu ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = UsuarioDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")})})
     @GetMapping(value = "/{id}")
     public ResponseEntity<UsuarioDTO> recover(@PathVariable("id") Long idUsuario) {
         return ResponseEntity.ok().body(usuarioService.recover(idUsuario));
     }
 
+    @Operation(
+            summary = "Deletar um usuário por ID",
+            description = "Este endpoint tem como objetivo deletar um usuário através do seu ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")})})
     @DeleteMapping(value = "/{id}")
     public ResponseEntity delete(@PathVariable("id") Long idUsuario) {
         usuarioService.delete(idUsuario);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @Operation(
+            summary = "Obter todos os usuários",
+            description = "Este endpoint tem como objetivo obter todos os usuários cadastrados no banco de dados.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = UsuarioDTO.class)))}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")})})
     @GetMapping
     public ResponseEntity<List<UsuarioDTO>> getAll() {
         return ResponseEntity.ok().body(usuarioService.getAll());
     }
 
+    @Operation(
+            summary = "Realizar login na aplicação",
+            description = "Este endpoint tem como objetivo reaizar a validação do usuário e senha enviados na requisição com os que estão cadastrados no banco.",
+            tags = {"Login"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = UsuarioDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "422", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ResponseExceptionDTO.class), mediaType = "application/json")})})
     @PostMapping(value = "/login")
     public ResponseEntity login(@RequestBody UsuarioDTO usuarioTO) {
         usuarioService.login(usuarioTO);
         return ResponseEntity.ok().build();
     }
+
 
 }
